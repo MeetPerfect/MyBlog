0c32a9829163ec853193ded53aa72d815c9adaf7
DeepFuture <12919034+gongkaiming@user.noreply.gitee.com>
2025-11-13 17:48:30 +0800

    自定义Jackson序列化和反序列化支持Java8新的日期API

--- weblog-web/src/main/java/com/kaiming/weblog/web/controller/TestController.java
+++ weblog-web/src/main/java/com/kaiming/weblog/web/controller/TestController.java
@@ -1,9 +1,12 @@
 package com.kaiming.weblog.web.controller;
 
 import com.kaiming.weblog.module.common.aspect.ApiOperationLog;
+import com.kaiming.weblog.module.common.utils.JsonUtil;
+import com.kaiming.weblog.module.common.utils.Response;
 import com.kaiming.weblog.web.model.User;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
@@ -12,6 +15,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.stream.Collectors;
 
 /**
@@ -25,25 +31,22 @@ import java.util.stream.Collectors;
  */
 @RestController
 @Api(tags = "首页模块")
+@Slf4j
 public class TestController {
 
 
     @PostMapping("/test")
     @ApiOperationLog(description = "测试接口")
     @ApiOperation(value = "测试接口")
-    public ResponseEntity<String> test(@RequestBody @Validated User user, BindingResult bindingResult) {
-        // 是否存在校验错误
-        if (bindingResult.hasErrors()) {
-            // 获取校验不通过字段的提示信息
-            String errorMsg = bindingResult.getFieldErrors()
-                    .stream()
-                    .map(FieldError::getDefaultMessage)
-                    .collect(Collectors.joining(", "));
+    public Response test(@RequestBody @Validated User user) {
+        // 打印入参
+        log.info(JsonUtil.toJsonString(user));
 
-            return ResponseEntity.badRequest().body(errorMsg);
-        }
+        // 设置三种日期字段值
+        user.setCreateTime(LocalDateTime.now());
+        user.setUpdateDate(LocalDate.now());
+        user.setTime(LocalTime.now());
 
-        // 返参
-        return ResponseEntity.ok("参数没有任何问题");
+        return Response.success(user);
     }
 }
