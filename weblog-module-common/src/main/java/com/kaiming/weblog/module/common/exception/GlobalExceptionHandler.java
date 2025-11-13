7499ee911c5ce2fe8ee359c26b37c4cdfbdbd32a
DeepFuture <12919034+gongkaiming@user.noreply.gitee.com>
2025-11-13 17:32:08 +0800

    全局异常处理&参数校验

--- weblog-module-common/src/main/java/com/kaiming/weblog/module/common/exception/GlobalExceptionHandler.java
+++ weblog-module-common/src/main/java/com/kaiming/weblog/module/common/exception/GlobalExceptionHandler.java
@@ -3,11 +3,14 @@ package com.kaiming.weblog.module.common.exception;
 import com.kaiming.weblog.module.common.enums.ResponseCodeEnum;
 import com.kaiming.weblog.module.common.utils.Response;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Optional;
 
 /**
  * ClassName: GlobalExceptionHandler
@@ -32,6 +35,42 @@ public class GlobalExceptionHandler {
     public Response<Object> handleBizException(HttpServletRequest request, BizException e) {
         log.warn("{} request fail, errorCode: {}, errorMessage: {}", request.getRequestURI(), e.getErrorCode(), e.getErrorMessage());
         return Response.fail(e);
+    }
+
+    /**
+     * 捕获参数校验异常
+     * @return
+     */
+    @ExceptionHandler({ MethodArgumentNotValidException.class })
+    @ResponseBody
+    public Response<Object> handleMethodArgumentNotValidException(HttpServletRequest request, MethodArgumentNotValidException e) {
+        // 参数错误异常码
+        String errorCode = ResponseCodeEnum.PARAM_NOT_VALID.getErrorCode();
+
+        // 获取 BindingResult
+        BindingResult bindingResult = e.getBindingResult();
+
+        StringBuilder sb = new StringBuilder();
+
+        // 获取校验不通过的字段，并组合错误信息，格式为： email 邮箱格式不正确, 当前值: '123124qq.com';
+        Optional.ofNullable(bindingResult.getFieldErrors()).ifPresent(errors -> {
+            errors.forEach(error ->
+                    sb.append(error.getField())
+                            .append(" ")
+                            .append(error.getDefaultMessage())
+                            .append(", 当前值: '")
+                            .append(error.getRejectedValue())
+                            .append("'; ")
+
+            );
+        });
+
+        // 错误信息
+        String errorMessage = sb.toString();
+
+        log.warn("{} request error, errorCode: {}, errorMessage: {}", request.getRequestURI(), errorCode, errorMessage);
+
+        return Response.fail(errorCode, errorMessage);
     }
 
     /**
